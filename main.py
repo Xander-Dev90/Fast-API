0d1746ee97e296ccfc45d31d1509bab5e6ee538e
Xander-Dev90 <klipsoporte@gmail.com>
2026-02-27 22:38:30 +0100

    Add customer retrieval and listing endpoints to the API

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import zoneinfo
 from datetime import datetime
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from models import Customer,CustomerCreate ,Transaction, Invoice
 from db import SessionDep, create_all_tables
+from sqlmodel import select
 
 
 
@@ -33,6 +34,7 @@ async def time(iso_code: str):
 
 
 @app.post("/customers", response_model=Customer)
+
 async def create_customer(customer_data: CustomerCreate, session: SessionDep):
     customer = Customer(**customer_data.model_dump())
     session.add(customer)
@@ -40,6 +42,17 @@ async def create_customer(customer_data: CustomerCreate, session: SessionDep):
     session.refresh(customer)
     return customer
 
+@app.get("/customers/{customer_id}", response_model=Customer)
+async def read_customer(customer_id: int, session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if not customer_db:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
+    return customer_db
+
+@app.get("/customers", response_model=list[Customer])
+async def list_customers(session: SessionDep):
+    customers = session.exec(select(Customer)).all()
+    return customers
 
 @app.post("/transactions")
 async def create_transation(transaction_data: Transaction, session: SessionDep):
