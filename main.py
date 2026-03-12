5249778207a1a1df9c9d570b6bfb85659d5f78e9
Xander-Dev90 <klipsoporte@gmail.com>
2026-03-12 21:59:53 +0100

    Se separa los endpoints del archivo main.py

--- main.py
+++ main.py
@@ -1,69 +1,10 @@
-import zoneinfo
-from datetime import datetime
-
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI
+from db import create_all_tables
+from venv_clean.app.main import app as api_router
 from models import Customer, CustomerCreate, Transaction, Invoice, CustomerUpdate
-from db import SessionDep, create_all_tables
-from sqlmodel import select
 
-
+# Crear la aplicación FastAPI
 app = FastAPI(lifespan=create_all_tables)
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hola, Persancho!"}
-
-
-country_timezones = {
-    "CO": "America/Bogota",
-    "MX": "America/Mexico_City",
-    "AR": "America/Argentina/Buenos_Aires",
-    "BR": "America/Sao_Paulo",
-    "PE": "America/Lima",
-}
-
-
-@app.get("/time/{iso_code}")
-async def time(iso_code: str):
-    iso = iso_code.upper()
-    timezone_str = country_timezones.get(iso)
-    tz = zoneinfo.ZoneInfo(timezone_str)
-    return {"time": datetime.now(tz)}
-
-
-@app.post("/customers", response_model=Customer)
-async def create_customer(customer_data: CustomerCreate, session: SessionDep):
-    customer = Customer(**customer_data.model_dump())
-    session.add(customer)
-    session.commit()
-    session.refresh(customer)
-    return customer
-
-
-@app.get("/customers/{customer_id}", response_model=Customer, status_code=status.HTTP_201_CREATED)
-async def read_customer(customer_id: int, session: SessionDep):
-    customer = session.query(Customer).filter(Customer.id == customer_id).first()
-    if not customer:
-        raise HTTPException(status_code=404, detail="Customer not found")
-    return customer
-
-
-@app.delete("/customers/{customer_id}", response_model=Customer)
-async def delete_customer(customer_id: int, session: SessionDep):
-    customer = session.query(Customer).filter(Customer.id == customer_id).first()
-    if not customer:
-        raise HTTPException(status_code=404, detail="Customer not found")
-    session.delete(customer)
-    session.commit()
-    return customer
-
-
-@app.post("/transactions")
-async def create_transation(transaction_data: Transaction, session: SessionDep):
-    return transaction_data
-
-
-@app.post("/invoices", response_model=Invoice)
-async def create_invoice(invoice_data: Invoice):
-    return invoice_data
+# Incluir el router de la API
+app.include_router(api_router)
